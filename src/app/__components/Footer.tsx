dae6fd414c0fe9922b89ef9a74af39321be4bfdb
gyeong deok <littleduck1219@gmail.com>
2024-01-18 22:50:06 +0900

    [Feat]header, footer select section

---
+++ src/app/__components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footerlink">
+      <a href="https://github.com/littleduck1219">littleduck</a>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/app/__components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.scss";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
