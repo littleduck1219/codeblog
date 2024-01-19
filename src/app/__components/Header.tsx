9a6c11e5a6506408f51899156eebdbfe147fa2f5
gyeong deok <littleduck1219@gmail.com>
2024-01-19 12:11:24 +0900

    [Design]header icon

--- src/app/__components/Header.tsx
+++ src/app/__components/Header.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./header.scss";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <div className="Header">
-      <div></div>
+      <div>
+        <Image
+          width={300}
+          height={80}
+          src="/img/littleduck-name-hight.png"
+          alt="title"
+        />
+      </div>
+      <div>
+        <Image
+          width={80}
+          height={80}
+          src="/img/littleduck-img-icon.png"
+          alt="profile-icon"
+        />
+      </div>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
-import styles from "./page.module.css";
+import "./page.scss";
 
 export default function Home() {
-  return <main className={styles.main}>dfsdfsdf</main>;
+  return (
+    <main className="home">
+      <div className="home__leftSectionWrapper">
+        <section className="home__leftSection">
+          <div className="home__leftSectionFixed">
+            <div>
+              <div>방명록</div>
+            </div>
+            <div>글쓰기</div>
+            <div>글쓰기</div>
+            <div>글쓰기</div>
+            <div>글쓰기</div>
+          </div>
+        </section>
+      </div>
+      <div className="home__rightSectionWrapper">
+        <div>post</div>
+        <div>post</div>
+        <div>post</div>
+        <div>post</div>
+        <div>post</div>
+        <div>post</div>
+        <div>post</div>
+      </div>
+    </main>
+  );
 }
