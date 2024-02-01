6a84d3b9d7e26b987e489d1c658a3d074d5797df
gyeong deok <littleduck1219@gmail.com>
2024-02-01 20:09:55 +0900

    [fix]memory maximum control

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    IS_DEV_MODE: process.env.IS_DEV_MODE === 'true' ? 'true' : 'false',
-  },
+
 };
 
 module.exports = nextConfig;
